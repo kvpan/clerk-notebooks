9a461b55430ab8939824bf5b9a593c0f23a667f0
Alex Gravem <alex@munnin.lan>
2024-11-11 02:39:38 +0000

    setup

---
+++ notebooks/moira.clj
@@ -0,0 +1,58 @@
+(ns moira
+  {:nextjournal.clerk/visibility {:code :hide :result :show}}
+  (:require [nextjournal.clerk :as clerk]
+            [meta-csv.core :as csv]
+            [tablecloth.api :as tc]))
+
+;; # Official WHO data on newborn growth
+;; Source: https://www.who.int/tools/child-growth-standards/standards/
+
+;; ## Weight
+(let [expected-weight (tc/dataset (csv/read-csv "notebooks/weight-zscore.csv"))
+      week (:Week expected-weight)
+      moira [2.640 2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
+  (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
+                        {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
+                        {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
+                        {:x week :y (into [] (:SD3neg expected-weight)) :type "scatter" :mode "lines" :name "SD -3"}
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                 :layout {:title "Z-score for weight-for-age"
+                          :xaxis {:title "Weeks"}
+                          :yaxis {:title "Weight (kg)"}}}))
+
+(let [expected-weight (tc/dataset (csv/read-csv "notebooks/weight-percentiles.csv"))
+      week (:Week expected-weight)
+      moira [2.640 2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
+  (clerk/plotly {:data [{:x week :y (into [] (:P50 expected-weight)) :type "scatter" :mode "lines" :name "50"}
+                        {:x week :y (into [] (:P10 expected-weight)) :type "scatter" :mode "lines" :name "10"}
+                        {:x week :y (into [] (:P5 expected-weight)) :type "scatter" :mode "lines" :name "5"}
+                        {:x week :y (into [] (:P3 expected-weight)) :type "scatter" :mode "lines" :name "3"}
+                        {:x week :y (into [] (:P1 expected-weight)) :type "scatter" :mode "lines" :name "1"}
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                 :layout {:title "Percentile for weight-for-age"
+                          :xaxis {:title "Weeks"}
+                          :yaxis {:title "Weight (kg)"}}}))
+;; ## Height
+(let [expected-weight (tc/dataset (csv/read-csv "notebooks/height.csv"))
+      week (:Week expected-weight)
+      moira [48 48.5 50 51.5 nil nil nil 52.5]]
+  (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
+                        {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
+                        {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                 :layout {:title "Z-score for height-for-age"
+                          :xaxis {:title "Weeks"}
+                          :yaxis {:title "Height (cm)"}}}))
+
+;; ## Head circumference
+(let [expected-weight (tc/dataset (csv/read-csv "notebooks/head.csv"))
+      week (:Week expected-weight)
+      moira [nil 34 35 35 nil nil 36 38]]
+  (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
+                        {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
+                        {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                 :layout {:title "Z-score for height-for-age"
+                          :xaxis {:title "Weeks"}
+                          :yaxis {:title "Height (cm)"}}}))
+
---
+++ src/user.clj
@@ -0,0 +1,18 @@
+(ns user
+  (:require [nextjournal.clerk :as clerk]))
+
+;; start Clerk's built-in webserver on the default port 7777, opening the browser when done
+;; (clerk/serve! {:browse true})
+
+;; either call `clerk/show!` explicitly
+;; (clerk/show! "notebooks/rule_30.clj")
+
+;; or let Clerk watch the given `:paths` for changes
+(clerk/serve! {:browse true :watch-paths ["notebooks"]})
+
+;; start with watcher and show filter function to enable notebook pinning
+;; (clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
+
+;; Build a html file from the given notebook notebooks.
+;; See the docstring for more options.
+;; (clerk/build! {:paths ["notebooks/rule_30.clj"]})
