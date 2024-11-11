ebd250a6a5403f68d7a93851af3d482d0fb69564
Alex Gravem <kartesus@gmail.com>
2024-11-11 17:57:58 +0000

    fix moira weight

--- notebooks/moira.clj
+++ notebooks/moira.clj
@@ -10,25 +10,26 @@
 ;; ## Weight
 (let [expected-weight (tc/dataset (csv/read-csv "notebooks/weight-zscore.csv"))
       week (:Week expected-weight)
-      moira [2.640 2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
+      moira [2.640 nil  2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
   (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
                         {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
                         {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
                         {:x week :y (into [] (:SD3neg expected-weight)) :type "scatter" :mode "lines" :name "SD -3"}
-                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira" :marker {:size 7 :color "#666"}}]
                  :layout {:title "Z-score for weight-for-age"
                           :xaxis {:title "Weeks"}
                           :yaxis {:title "Weight (kg)"}}}))
 
 (let [expected-weight (tc/dataset (csv/read-csv "notebooks/weight-percentiles.csv"))
       week (:Week expected-weight)
-      moira [2.640 2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
+      moira [2.640 nil 2.690 2.770 2.960 3.120 3.250 3.320 3.770]]
   (clerk/plotly {:data [{:x week :y (into [] (:P50 expected-weight)) :type "scatter" :mode "lines" :name "50"}
                         {:x week :y (into [] (:P10 expected-weight)) :type "scatter" :mode "lines" :name "10"}
                         {:x week :y (into [] (:P5 expected-weight)) :type "scatter" :mode "lines" :name "5"}
                         {:x week :y (into [] (:P3 expected-weight)) :type "scatter" :mode "lines" :name "3"}
                         {:x week :y (into [] (:P1 expected-weight)) :type "scatter" :mode "lines" :name "1"}
-                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                        {:x week :y (into [] (:P01 expected-weight)) :type "scatter" :mode "lines" :name "0.1"}
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira" :marker {:size 7 :color "#666"}}]
                  :layout {:title "Percentile for weight-for-age"
                           :xaxis {:title "Weeks"}
                           :yaxis {:title "Weight (kg)"}}}))
@@ -39,7 +40,7 @@
   (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
                         {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
                         {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
-                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira" :marker {:size 7 :color "#666"}}]
                  :layout {:title "Z-score for height-for-age"
                           :xaxis {:title "Weeks"}
                           :yaxis {:title "Height (cm)"}}}))
@@ -51,7 +52,7 @@
   (clerk/plotly {:data [{:x week :y (into [] (:SD0 expected-weight)) :type "scatter" :mode "lines" :name "Median"}
                         {:x week :y (into [] (:SD1neg expected-weight)) :type "scatter" :mode "lines" :name "SD -1"}
                         {:x week :y (into [] (:SD2neg expected-weight)) :type "scatter" :mode "lines" :name "SD -2"}
-                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira"}]
+                        {:x week :y moira :type "scatter" :mode "markers" :name "Moira" :marker {:size 7 :color "#666"}}]
                  :layout {:title "Z-score for height-for-age"
                           :xaxis {:title "Weeks"}
                           :yaxis {:title "Height (cm)"}}}))
